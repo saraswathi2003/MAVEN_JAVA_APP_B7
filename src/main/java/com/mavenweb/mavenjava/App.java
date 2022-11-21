ab6d86d9bc70f94b73ba5373173c8ac62666abbd
saraswathi <saraswathi2003@gmail.com>
2022-11-21 21:07:16 +0530

    first maven java app

---
+++ src/main/java/com/mavenweb/mavenjava/App.java
@@ -0,0 +1,13 @@
+package com.mavenweb.mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Welcome to java app" );
+    }
+}
